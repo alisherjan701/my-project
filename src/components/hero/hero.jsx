5e994875e45a3cd8b2f3a7518f861367c1daea8c
alisherjan701 <abdulayevalisher987@gmail.com>
2025-07-11 15:07:23 +0500

    heros

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <div className=' flex justify-center bg-[#C2EFD4] '>
         <div className='mt-[160px]'>
-            <h1 className='text-[#224F34] text-[60px] font-black w-[730px]'>O'z modangizni kashf eting va toping!</h1>
+            <h1 className='text-[#224F34] text-[60px] font-black w-[730px]'>O'z moodangizni kashf eting va toping!</h1>
         </div>
         <div>
             <img src="" alt="" />
